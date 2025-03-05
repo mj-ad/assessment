7248979d0623e5ac062d7fe6fc841ceb28cd2e38
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2025-03-05 14:55:26 +0100

    conent

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Request::macro('contentSize', function () {
+            return strlen($this->getContent());
+        });
+    
+        \Log::info('Request Size: ' . request()->contentSize() . ' bytes');
     }
 }
